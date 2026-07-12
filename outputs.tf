ba13623d00ee9048cce63453783deff379191f3e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "managed_applications" {
-  description = "All managed_application resources"
-  value       = azurerm_managed_application.managed_applications
-}
 output "managed_applications_application_definition_id" {
-  description = "List of application_definition_id values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.application_definition_id]
+  description = "Map of application_definition_id values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.application_definition_id }
 }
 output "managed_applications_kind" {
-  description = "List of kind values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.kind]
+  description = "Map of kind values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.kind }
 }
 output "managed_applications_location" {
-  description = "List of location values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.location]
+  description = "Map of location values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.location }
 }
 output "managed_applications_managed_resource_group_name" {
-  description = "List of managed_resource_group_name values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.managed_resource_group_name]
+  description = "Map of managed_resource_group_name values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.managed_resource_group_name }
 }
 output "managed_applications_name" {
-  description = "List of name values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.name]
+  description = "Map of name values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.name }
 }
 output "managed_applications_outputs" {
-  description = "List of outputs values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.outputs]
+  description = "Map of outputs values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.outputs }
 }
 output "managed_applications_parameter_values" {
-  description = "List of parameter_values values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.parameter_values]
+  description = "Map of parameter_values values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.parameter_values }
 }
 output "managed_applications_plan" {
-  description = "List of plan values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.plan]
+  description = "Map of plan values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.plan }
 }
 output "managed_applications_resource_group_name" {
-  description = "List of resource_group_name values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.resource_group_name]
+  description = "Map of resource_group_name values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.resource_group_name }
 }
 output "managed_applications_tags" {
-  description = "List of tags values across all managed_applications"
-  value       = [for k, v in azurerm_managed_application.managed_applications : v.tags]
+  description = "Map of tags values across all managed_applications, keyed the same as var.managed_applications"
+  value       = { for k, v in azurerm_managed_application.managed_applications : k => v.tags }
 }
 
